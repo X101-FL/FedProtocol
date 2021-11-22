ba827c09abd818bbd40904dd10c2cd28dcb22f57
ScottWong <jmhgchn@gmail.com>
2021-11-22 22:18:53 +0800

    feature: PSI

--- components/smc/psi/psi.py
+++ components/smc/psi/psi.py
@@ -1,100 +1,103 @@
 import random
 
-import numpy as np
-from Crypto.Hash import SHAKE256
-
-from components.smc.psi.cuckoo import CuckooHashTable
-from components.smc.tools.arr import rand_binary_arr
-from components.smc.tools.serialize import int_to_bytes, bytes_to_bit_arr
+from components.smc.psi.cuckoo import position_hash, CuckooHashTable
+from components.smc.psi.oprf import OPRFServer, OPRFClient
 from fedprototype import BaseClient
 
 
-class PSIClient(BaseClient):
+class PSIServer(BaseClient):
 
-    def run(self, **kwargs):
+    def __init__(self, words):
+        super().__init__("PSIServer")
+
+        self.s = 5
+        self.words = words
+
+        self.oprf_server = OPRFServer(512)
+
+    def init(self):
+        self.set_sub_client(self.oprf_server, role_rename_dict={"OPRFServer": "PSIServer",
+                                                                "OPRFClient": "PSIClient"})
+        self.oprf_server.init()
+
+    def run(self):
+        n = self.oprf_server.max_count - self.s
+        for i in range(3):
+            words = random.sample(self.words, len(self.words))
+            for word in words:
+                h = position_hash(i + 1, word, n)
+                val = self.oprf_server.run(h, word + bytes([i + 1]))
+                self.logger.debug(f"hash index {i + 1}, table position {h},"
+                                  f"word {int.from_bytes(word, 'big')}, val {val.hex()}")
+                self.comm.send(receiver="PSIClient", message_name="word", obj=val)
+            self.comm.send(receiver="PSIClient", message_name="word", obj=b"end")
+
+        for i in range(self.s):
+            words = random.sample(self.words, len(self.words))
+            for word in words:
+                val = self.oprf_server.run(n + i, word)
+                self.logger.debug(f"table position {n + i}, "
+                                  f"word {int.from_bytes(word, 'big')}, val {val.hex()}")
+                self.comm.send(receiver="PSIClient", message_name="word", obj=val)
+            self.comm.send(receiver="PSIClient", message_name="word", obj=b"end")
+
+        res = []
+        while True:
+            word = self.comm.receive(sender="PSIClient", message_name="word")
+            if word == b"end":
+                break
+            res.append(word)
+
+        return res
         pass
 
     def close(self):
         pass
 
-    def __init__(self, words, role_name):
-        """
-        :param words: List[bytes]
-        """
-        super().__init__(role_name)
 
-        self.n, self.table, self.stash, self.table_hash_index, dummy_table = self._init_cuckoo(words)
+class PSIClient(BaseClient):
 
-        self._codewords = 512
-
-        self._r = np.empty((len(dummy_table), self._codewords), dtype=np.uint8)
-        codewords_bytes = int_to_bytes(self._codewords)
-        for i, word in enumerate(dummy_table):
-            word_arr = bytes_to_bit_arr(codewords_bytes + self._encode(word))
-            self._r[i] = word_arr
-
-    def init(self):
-        m = self._r.shape[0]
-        self._t = rand_binary_arr((m, self._codewords))
-        u = self._t ^ self._r
-
-        self._s = rand_binary_arr()
-        # if self._codewords == 128:
-        #     for i in range(self._codewords):
-        #         ti_bytes = bit_arr_to_bytes(self._t[:, i])
-        #         ui_bytes = bit_arr_to_bytes(u[:, i])
-        #         base.send(self._pair, ti_bytes, ui_bytes)
-        # else:
-        #     sender = Sender(self._pair, 128)
-        #     sender.prepare()
-        #
-        #     for i in range(self._codewords):
-        #         ti_bytes = bit_arr_to_bytes(self._t[:, i])
-        #         ui_bytes = bit_arr_to_bytes(u[:, i])
-        #         sender.send(ti_bytes, ui_bytes)
-        # self._pair.barrier()
-
-    def _encode(self, data):
-        shake = SHAKE256.new(data)
-        length = (self._codewords + 7) // 8
-        return shake.read(length)
-
-    @classmethod
-    def _init_cuckoo(cls, words):
-        n = int(1.2 * len(words))
+    def __init__(self, words):
+        super().__init__("PSIClient")
+        self.n = int(1.2 * len(words))
         s = 5
-        cuckoo = CuckooHashTable(n, s)
+        cuckoo = CuckooHashTable(self.n, s)
         for word in words:
             cuckoo.update(word)
-        table = cuckoo.table
-        stash = cuckoo.stash
-        table_hash_index = cuckoo.table_hash_index
+        self.table = cuckoo.table
+        self.stash = cuckoo.stash
+        self.table_hash_index = cuckoo.table_hash_index
 
         dummy_table = []
-        for key, hash_index in zip(table, table_hash_index):
+        for key, hash_index in zip(self.table, self.table_hash_index):
             if key is None:
                 key = random.getrandbits(64).to_bytes(8, "big")
             else:
                 key = key + bytes([hash_index])
             dummy_table.append(key)
-        for key in stash:
+        for key in self.stash:
             if key is None:
                 key = random.getrandbits(64).to_bytes(8, "big")
             dummy_table.append(key)
-        return n, table, stash, table_hash_index, dummy_table
 
+        self.oprf_client = OPRFClient(dummy_table, 512)
 
-    def intersect(self) -> List[bytes]:
+    def init(self):
+        self.set_sub_client(self.oprf_client, role_rename_dict={"OPRFServer": "PSIServer",
+                                                                "OPRFClient": "PSIClient"})
+        self.oprf_client.init()
+
+    def run(self):
         peer_tables = [{}, {}, {}]
         peer_stash = {}
         for table in peer_tables:
             while True:
-                key = self.pair.recv()
+                key = self.comm.receive(sender="PSIServer", message_name="word")
                 if key == b"end":
                     break
                 table[key] = None
         while True:
-            key = self.pair.recv()
+            key = self.comm.receive(sender="PSIServer", message_name="word")
             if key == b"end":
                 break
             peer_stash[key] = None
@@ -103,20 +106,23 @@ class PSIClient(BaseClient):
         for i, (key, hash_index) in enumerate(zip(self.table, self.table_hash_index)):
             if key is not None:
                 peer_table = peer_tables[hash_index - 1]
-                local_val = self.oprf_client.eval(i)
-                _logger.debug(f"hash index {hash_index}, table position {i}, "
-                              f"word {int.from_bytes(key, 'big')}, val {local_val.hex()}")
+                local_val = self.oprf_client.run(i)
+                self.logger.debug(f"hash index {hash_index}, table position {i}, "
+                                  f"word {int.from_bytes(key, 'big')}, val {local_val.hex()}")
                 if local_val in peer_table:
                     res.append(key)
-                    self.pair.send(key)
+                    self.comm.send(receiver="PSIServer", message_name="word", obj=key)
 
         for i, key in enumerate(self.stash):
             if key is not None:
-                local_val = self.oprf_client.eval(i + self.n)
-                _logger.debug(f"table position {i + self.n}, "
+                local_val = self.oprf_client.run(i + self.n)
+                self.logger.debug(f"table position {i + self.n}, "
                               f"word {int.from_bytes(key, 'big')}, val {local_val.hex()}")
                 if local_val in peer_stash:
                     res.append(key)
-                    self.pair.send(key)
-        self.pair.send(b"end")
+                    self.comm.send(receiver="PSIServer", message_name="word", obj=key)
+        self.comm.send(receiver="PSIServer", message_name="word", obj=b"end")
         return res
+
+    def close(self):
+        pass
---
+++ jobs/sample/local_psi.py
@@ -0,0 +1,114 @@
+import random
+import time
+
+import numpy as np
+
+from components.smc.psi.oprf import OPRFServer, OPRFClient
+from components.smc.psi.ote import OTESender, OTEReceiver
+from components.smc.psi.psi import PSIServer, PSIClient
+from fedprototype.base_client import BaseClient
+
+
+class ActiveClient(BaseClient):
+
+    def __init__(self):
+        super().__init__("Active")
+        # self.ote_sender = OTESender(256)
+
+        # self.oprf_server = OPRFServer(512)
+
+        words = [i.to_bytes(4, "big") for i in range(500, 1000)]  # local sets, should be a list of bytes
+        self.psi_server = PSIServer(words)
+
+    def init(self):
+        # self.set_sub_client(self.ote_sender, role_rename_dict={"OTESender": "Active", "OTEReceiver": "Passive"})
+        # self.set_sub_client(self.oprf_server, role_rename_dict={"OPRFServer": "Active", "OPRFClient": "Passive"})
+        self.set_sub_client(self.psi_server, role_rename_dict={"PSIServer": "Active", "PSIClient": "Passive"})
+
+    def run(self):
+        self.psi_server.init()
+        self.logger.debug(f"PSIServer prepare complete")
+
+        self.logger.debug(f"Start intersection")
+        res = self.psi_server.run()  # intersect stage, res is the intersection
+        self.logger.debug(f"Finish intersection")
+        ans = sorted([int.from_bytes(val, "big") for val in res])
+        self.logger.info(f"result is {ans}")
+
+        # self.oprf_server.init()
+        # self.logger.debug(f"OPRFServer prepare complete")
+        # for i in range(1000):
+        #     word = i.to_bytes(4, "big")
+        #     res = self.oprf_server.run(i, word)
+        #     self.comm.send(receiver="Passive", message_name="item", obj=res)
+        #     self.logger.debug(f"{i} {res.hex()}")
+
+        # self.ote_sender.init()
+        # self.logger.debug(f"OTESender prepare complete")
+        #
+        # for i in range(self.ote_sender.max_count):
+        #     m0 = (str(i) + '0').encode("utf-8")
+        #     m1 = (str(i) + '1').encode("utf-8")
+        #
+        #     self.ote_sender.run(m0, m1)
+
+    def close(self):
+        pass
+
+
+class PassiveClient(BaseClient):
+
+    def __init__(self):
+        super().__init__("Passive")
+        # self.r = [random.randint(0, 1) for _ in range(1000)]
+        # self.ote_receiver = OTEReceiver(self.r, 256)
+
+        # words = [i.to_bytes(4, "big") for i in range(1000)]
+        # self.oprf_client = OPRFClient(words, 512)
+
+        words = [i.to_bytes(4, "big") for i in range(1000)]  # local sets, should be a list of bytes
+        self.psi_client = PSIClient(words)
+
+    def init(self):
+        # self.set_sub_client(self.ote_receiver, role_rename_dict={"OTESender": "Active", "OTEReceiver": "Passive"})
+        # self.set_sub_client(self.oprf_client, role_rename_dict={"OPRFServer": "Active", "OPRFClient": "Passive"})
+        self.set_sub_client(self.psi_client, role_rename_dict={"PSIServer": "Active", "PSIClient": "Passive"})
+
+    def run(self):
+        self.psi_client.init()
+        self.logger.debug(f"PSIClient prepare complete")
+
+        self.logger.debug(f"Start intersection")
+        res = self.psi_client.run()  # intersect stage, res is the intersection
+        self.logger.debug(f"Finish intersection")
+        ans = sorted([int.from_bytes(val, "big") for val in res])
+        self.logger.info(f"Results is {ans}")
+
+        # self.oprf_client.init()
+        # self.logger.debug(f"OPRFClient prepare complete")
+        #
+        # for i in range(1000):
+        #     peer_val = self.comm.receive(sender="Active", message_name="item").hex()
+        #     local_val = self.oprf_client.run(i).hex()
+        #     self.logger.debug(f"{i} {local_val} {peer_val}")
+        #     assert local_val == peer_val, f"{i}th oprf is not correct"
+
+        # self.ote_receiver.init()
+        # self.logger.debug(f"OTEReceiver prepare complete")
+        #
+        # for i, b in enumerate(self.r):
+        #     msg = self.ote_receiver.run().decode("utf-8")
+        #     self.logger.debug(f"msg is: {msg[:-1]} {msg[-1]}")
+        #     assert msg == str(i) + str(b)
+
+    def close(self):
+        pass
+
+
+if __name__ == '__main__':
+    from fedprototype.envs import LocalEnv
+
+    env = LocalEnv()
+    env.add_client(client=ActiveClient())
+    env.add_client(client=PassiveClient())
+    env.run()
